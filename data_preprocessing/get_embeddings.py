37af161bf52957c371f8e407ec6f6290ecc23353
GurevichPE <gurevich.pe@phystech.edu>
2025-04-30 16:10:05 +0000

    preprocessing

---
+++ data_preprocessing/get_embeddings.py
@@ -0,0 +1,57 @@
+"""
+dataset was taken from https://www.kaggle.com/datasets/thedrcat/daigt-v2-train-dataset
+"""
+
+from transformers import AutoTokenizer, AutoModel
+import torch
+import pandas as pd
+
+# Load model and tokenizer (e.g., BERT)
+
+
+
+def main():
+    print("START")
+
+    DATA_PATH = "/workspace/mnt/local/data/pgurevich/magnitude/small_data.csv"
+    SAVEDIR = "/workspace/mnt/local/data/pgurevich/magnitude"
+
+    TEXT_COLUMN = "text"
+    LABEL_COLUMN = "label"
+
+    DEVICE = torch.device(f"cuda:0" if torch.cuda.is_available() else "cpu")
+
+    data = pd.read_csv(DATA_PATH)
+    texts = data[TEXT_COLUMN].tolist()
+    labels = data[LABEL_COLUMN].to_numpy()
+
+    model_name = "bert-base-uncased"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModel.from_pretrained(model_name).to(DEVICE)
+
+    inputs = tokenizer(
+        texts,
+        padding=True,
+        truncation=True,
+        return_tensors="pt"  # Return PyTorch tensors
+    ).to(DEVICE)
+
+    with torch.no_grad():
+        outputs = model(**inputs)
+
+    # Extract embeddings (shape: [batch_size, sequence_length, hidden_size])
+    token_embeddings = outputs.last_hidden_state.cpu()
+
+    labels = torch.from_numpy(labels)
+
+    print("FINISHED")
+    print(f"Embeddings shape: {token_embeddings.shape}")
+
+    torch.save(token_embeddings, f"{SAVEDIR}/embeddings.pt")
+    torch.save(labels, f"{SAVEDIR}/labels.pt")
+
+
+if __name__ == "__main__":
+    main()
+
+
