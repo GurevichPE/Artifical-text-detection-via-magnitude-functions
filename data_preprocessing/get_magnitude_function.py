4de1a57df4c8d125eb1f15cd51dee53f0f1e86e3
GurevichPE <gurevich.pe@phystech.edu>
2025-05-17 12:19:48 +0000

    classifier and exps

--- data_preprocessing/get_magnitude_function.py
+++ data_preprocessing/get_magnitude_function.py
@@ -9,27 +9,28 @@ sys.path.insert(0, "..")
 from magnitude.magnitude_function import calculate_magnitude_function
 
 def main():
-    EMB_PATH = "/workspace/mnt/local/data/pgurevich/magnitude/embeddings.pt"
+    EMB_PATH = "/workspace/mnt/local/data/pgurevich/magnitude"
     SAVEDIR = "/workspace/mnt/local/data/pgurevich/magnitude"
     T_MIN = 0.001
     T_MAX = 0.1
-    N_STEPS = 500
+    N_STEPS = 200
     METRIC = 'cityblock'
     N_JOBS = -1
 
-    embs = torch.load(EMB_PATH).numpy()
     t = np.linspace(T_MIN, T_MAX, N_STEPS)
-    
-    # Parallel computation
-    mfs = Parallel(n_jobs=N_JOBS)(
-        delayed(calculate_magnitude_function)(x, METRIC, t) 
-        for x in embs
-    )
-    
-    with open(f"{SAVEDIR}/magnitude_f.pkl", 'wb+') as f:
-        pkl.dump(mfs, f)
+    for i in range(64, 321, 32):
+        embs = torch.load(f"{EMB_PATH}/embeddings_{i}.pt").numpy()
+        
+        # Parallel computation
+        mfs = Parallel(n_jobs=N_JOBS)(
+            delayed(calculate_magnitude_function)(x, METRIC, t) 
+            for x in embs
+        )
+        
+        with open(f"{SAVEDIR}/magnitude_f_{i}.pkl", 'wb+') as f:
+            pkl.dump(mfs, f)
 
-    with open(f"{SAVEDIR}/t_grid.pkl", 'wb+') as f:
+    with open(f"{SAVEDIR}/t_grid_multiple.pkl", 'wb+') as f:
         pkl.dump(t, f)
 
 if __name__ == "__main__":
