cd9fdd7299c30b0c8252208fd0d702b5efcf9d19
GurevichPE <gurevich.pe@phystech.edu>
2025-05-18 20:42:31 +0000

    last breath

--- magnitude_classifier/train.py
+++ magnitude_classifier/train.py
@@ -5,35 +5,36 @@ from tqdm import tqdm
 import torch
 
 def main():
-    SAVEDIR = "/workspace/mnt/local/data/pgurevich/magnitude"
-    MAGNITUDE_PATH = "/workspace/mnt/local/data/pgurevich/magnitude"
-    DATA_PATH = "/workspace/mnt/local/data/pgurevich/magnitude/small_data.csv"
-    LABELS_PATH = "/workspace/mnt/local/data/pgurevich/magnitude/labels.pt"
+    SAVEDIR =         "/workspace/mnt/local/data/pgurevich/magnitude"
+    MAGNITUDE_PATH =  "/workspace/mnt/local/data/pgurevich/magnitude"
+    EMBEDDINGS_PATH = "/workspace/mnt/local/data/pgurevich/magnitude"
+    DATA_PATH =       "/workspace/mnt/local/data/pgurevich/magnitude/small_data.csv"
+    LABELS_PATH =     "/workspace/mnt/local/data/pgurevich/magnitude/labels.pt"
     MODE = "both"       # 'magnitude' for magnitude function or
                         # 'embeddings' for embeddings or
                         # 'both' for concatenated embeddings and magnitudes
 
-    LEN_GRID = [288]
+    MAX_TOKEN_LENGTHS = [288]
     N_STEPS = 300
     
     labels, data = load_data(LABELS_PATH, DATA_PATH)
     aucs = []
     misclasses = []
 
-    for n in tqdm(LEN_GRID):
+    for n in tqdm(MAX_TOKEN_LENGTHS):
         if MODE == 'magnitude':
             with open(f"{MAGNITUDE_PATH}/magnitude_f_{n}_{N_STEPS}_steps.pkl", 'rb') as f:
                 mags = pkl.load(f)
             mags = np.array(mags)
         elif MODE == 'embeddings':
-            mags = torch.load(f"{MAGNITUDE_PATH}/embeddings_{n}.pt")
+            mags = torch.load(f"{EMBEDDINGS_PATH}/embeddings_{n}.pt")
             mags = mags.mean(dim=1).numpy()
 
         elif MODE == 'both':
             with open(f"{MAGNITUDE_PATH}/magnitude_f_{n}_{N_STEPS}_steps.pkl", 'rb') as f:
                 mags = pkl.load(f)
             mags = np.array(mags)
-            embs = torch.load(f"{MAGNITUDE_PATH}/embeddings_{n}.pt")
+            embs = torch.load(f"{EMBEDDINGS_PATH}/embeddings_{n}.pt")
             embs = embs.mean(dim=1).numpy()
             mags = np.concatenate([embs, mags], axis=1)
 
@@ -48,7 +49,7 @@ def main():
     final_result = {
         "aucs" : aucs,
         "mistakes" : misclasses,
-        "len_grid" : LEN_GRID
+        "len_grid" : MAX_TOKEN_LENGTHS
     }
 
     with open(f"{SAVEDIR}/clf_results_{MODE}_final.pkl", "wb") as f:
